079acddb945d0c6ebc45328a139cc133c6d3ddad
“[jill <[jillpatel1692@gmail.com]>
2022-09-29 10:26:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const hello = () => {
+    console.log("Hello Node.js!");
+  };
+  
+  hello();
+ /* terminal
+ 1 + 1
+ x = 1 + 2
+  x + 4
+  x * 4
+  "john doe"
+"my name is : ${_}" 
+ */ 
+let x = 1 + 2 ;
+console.log(x);
+  console.log("hello world");
+  const multiline = () => {
+    console.log("multi line code block");
+  };
+  multiline(); // print
+  
