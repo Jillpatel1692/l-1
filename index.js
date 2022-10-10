cdc57b916a3524423ff123cc9898f9fe07cddc3e
“[jill <[jillpatel1692@gmail.com]>
2022-10-10 09:40:39 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-const hello = () => {
-    console.log("Hello Node.js!");
-  };
-  
-  hello();
- /* terminal
- 1 + 1
- x = 1 + 2
-  x + 4
-  x * 4
-  "john doe"
-"my name is : ${_}" 
- */ 
-let x = 1 + 2 ;
-console.log(x);
-  console.log("hello world");
-  const multiline = () => {
-    console.log("multi line code block");
-  };
-  multiline(); // print
-  
+let hello = "Hello Node.js!";
+returnHello = () => {
+  console.log("Hello Node.js!")
+}
+returnHello();
